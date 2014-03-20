1f72b9a04d77ab8e3440fe050ae9f2752b6782a2
Hannah Bottalla <hannah@instructure.com>
2014-03-19 19:27:35 -0600

    User registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :avatar, :remember_me, :provider, :uid, :email_favorites) }
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email) }
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :username) }
     end
 
 end
